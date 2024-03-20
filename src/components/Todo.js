93e8d6094e8479c22a525bc2da5a6f306987143a
chiedozie <chiedozieezenwa@gmail.com>
2024-03-20 12:30:12 +0100

    to-do list developed with reactJS

---
+++ src/components/Todo.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Todo.css";
+
+export const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
+  return (
+    <div className="todo">
+      <p
+        onClick={() => toggleComplete(task.id)}
+        className={`$(task.completed ? 'completed': "")`}
+      >
+        {task.task}
+      </p>
+      <div>
+        <button onClick={() => editTodo(task.id)} className="update-btn">
+          update task
+        </button>
+        <button onClick={() => deleteTodo(task.id)} className="delete-btn">
+          delete
+        </button>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/TodoFooter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const TodoFooter = () => {
+  return (
+    <div>
+      <p className="Footer">
+        &copy; 2024 All rights reserved|Designed by Chiedozie Ezenwa
+      </p>
+    </div>
+  );
+};
